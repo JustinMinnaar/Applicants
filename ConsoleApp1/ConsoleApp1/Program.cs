5d16305c08b24aeebdd156de1767829e64fd9cda
Justin Minnaar <Justin@KnightsIT.co.za>
2021-01-06 11:01:16 +0200

    Added example of calculating age from dates to console app.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,11 +2,18 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var b = new DateTime(1969, 7, 31);
+            var dToday = (DateTime.Now.Year * 12 + DateTime.Now.Month);
+            var dBirthdate = (b.Year * 12 + b.Month);
+            var yy = (dToday - dBirthdate) / 12;
+
+            Console.WriteLine(yy);
         }
     }
 }
